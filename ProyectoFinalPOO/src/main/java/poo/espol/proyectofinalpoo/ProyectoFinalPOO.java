f717e4091359dafcb00c67ef9efc096dcc387f63
Carlos Flores <carfgonz@espol.edu.ec>
2022-12-19 17:36:35 -0500

    Commit de prueba

--- ProyectoFinalPOO/src/main/java/poo/espol/proyectofinalpoo/ProyectoFinalPOO.java
+++ ProyectoFinalPOO/src/main/java/poo/espol/proyectofinalpoo/ProyectoFinalPOO.java
@@ -14,5 +14,6 @@ public class ProyectoFinalPOO {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Prueba");
+        System.out.println("Pruba2");
     }
 }
