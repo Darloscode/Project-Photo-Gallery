ca8c365efb2cb31ba1f844050b42bbdb7a9c8d4b
Carlos Flores <carfgonz@espol.edu.ec>
2022-12-20 17:21:55 -0500

    Cambios

--- ProyectoFinalPOO/src/main/java/poo/espol/proyectofinalpoo/ProyectoFinalPOO.java
+++ ProyectoFinalPOO/src/main/java/poo/espol/proyectofinalpoo/ProyectoFinalPOO.java
@@ -1,5 +1,8 @@
 package poo.espol.proyectofinalpoo;
 
+import java.awt.Image;
+import java.util.*;
+
 /**
  * @author Aaron Macias Catagua
  * @author Cristhian Rodriguez Villegas
@@ -10,5 +13,30 @@ public class ProyectoFinalPOO {
 
     public static void main(String[] args) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        //Pruebas
+        //Image im = new Image();
+        Calendar cd = new GregorianCalendar(1344, Calendar.DECEMBER, 12);
+        System.out.println(cd.get(Calendar.DATE));
+
+    }
+    
+    public static void inicarSistema(){
     }
 }
