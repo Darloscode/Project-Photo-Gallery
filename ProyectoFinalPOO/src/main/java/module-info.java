19b99caabb1e8bb80241fdbb46db3e803b9ffab0
Carlos Flores <carfgonz@espol.edu.ec>
2023-01-07 18:16:15 -0500

    Crear ventanas

---
+++ ProyectoFinalPOO/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module espol.poo.proyectofinalpoo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens espol.poo.proyectofinalpoo to javafx.fxml;
+    exports espol.poo.proyectofinalpoo;
+}
